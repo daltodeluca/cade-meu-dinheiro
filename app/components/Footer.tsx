72e4f944856df2460550353964810ac82f950f30
Vinícius de Luca <viniciusdluca@Viniciuss-MacBook-Air.local>
2026-03-26 15:06:21 -0300

    chore: setup deployment configurations closes #2

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className="mt-16 pb-8 text-center">
       <div className="inline-block p-1">
         <span className="text-slate-400 dark:text-slate-600 text-[10px] font-black uppercase tracking-[0.5em] select-none">
-          InLuce Technologies © {year}
+          Dalto de Luca, InLuce Technologies © {year}
         </span>
       </div>
     </footer>
